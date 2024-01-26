7b9056886a118a8d3ea6f8d4a2839817f2bac798
Ian Bailey <IanKulin@kulin.com.au>
2024-01-26 13:31:08 +0800

    feat: session saving

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const app = express();
 
-app.use(session({
-  secret: 'your-secret-key', // This should be a secret, used to sign the session ID cookie
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "your-secret-keyz", // This should be a secret, used to sign the session ID cookie
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore,
+  })
+);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // Access session data
   if (req.session.views) {
     req.session.views++;
@@ -21,5 +25,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
